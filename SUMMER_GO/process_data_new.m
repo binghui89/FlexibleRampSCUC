6349d2f785bd9ca4796495e35ab5f41837b620b0
binghui89 <bli6@ncsu.edu>
2020-12-12 16:54:42 -0600

    Complete processing wind and solar data (no DA solar).

--- SUMMER_GO/process_data_new.m
+++ SUMMER_GO/process_data_new.m
@@ -1,13 +1,18 @@
 % For the file from Cong in the second time 
+%% Read wind data
 
-T_wind = readtable('Cong/Forecasts2Binghui/Wind_debug/WindBus_Forecasts_debug.csv');
-T_wind.leadtime = T_wind.TimeStamp - T_wind.IssueTime;
-unique_leadtime_wind = unique(T_wind.leadtime);
+Tf_wind_ha = readtable('Cong/Forecasts2Binghui/Wind_debug/WindBus_Forecasts_debug.csv');
+Tf_wind_ha.leadtime = Tf_wind_ha.TimeStamp - Tf_wind_ha.IssueTime;
+unique_leadtime_wind = unique(Tf_wind_ha.leadtime);
 for i = 1:length(unique_leadtime_wind)
-    tmp = T_wind(T_wind.leadtime==unique_leadtime_wind(i), :);
-    fprintf('Leadtime: %s, %g in total.\n', unique_leadtime_wind(i), size(tmp, 1));
+    tmp = Tf_wind_ha(Tf_wind_ha.leadtime==unique_leadtime_wind(i), :);
+    fprintf('Solar, Leadtime: %s, %g in total.\n', unique_leadtime_wind(i), size(tmp, 1));
 end
 
+Tf_wind_da = readtable('Cong/Forecasts2Binghui/Wind_debug/WindBus_ForecastDA.csv');
+Ta_wind = readtable('Cong/Forecasts2Binghui/Wind_debug/WindBus_Actual.csv');
+
+%% Read solar data
 genname_solar = {
     'solar109';
     'solar21';
@@ -52,40 +57,41 @@ for j = 1: numel(genname_solar)
     ar_forecast(:, j) = T_solar.Forecast;
     ar_actual(:, j) = T_solar.Actual;
 end
-Tf_solar = [T_solar(:, {'TimeStamp', 'IssueTime'}) array2table(ar_forecast, 'VariableNames', genname_solar)];
+Tf_solar_ha = [T_solar(:, {'TimeStamp', 'IssueTime'}) array2table(ar_forecast, 'VariableNames', genname_solar)];
 Ta_solar = [T_solar(:, {'TimeStamp', 'IssueTime'}) array2table(ar_actual, 'VariableNames', genname_solar)];
-Tf_solar.leadtime = Tf_solar.TimeStamp - Tf_solar.IssueTime;
+Tf_solar_ha.leadtime = Tf_solar_ha.TimeStamp - Tf_solar_ha.IssueTime;
 Ta_solar.leadtime = Ta_solar.TimeStamp - Ta_solar.IssueTime;
 
 %% Check missing values for a specific lead time
 % t_min = min([min(T_load.TimeStamp), min(T_wind.TimeStamp), min(T_solar.TimeStamp)]);
 % t_max = max([max(T_load.TimeStamp), max(T_wind.TimeStamp), max(T_solar.TimeStamp)]);
-t_min = min([min(T_wind.TimeStamp), min(Tf_solar.TimeStamp)]);
-t_max = max([max(T_wind.TimeStamp), max(Tf_solar.TimeStamp)]);
-time_seq = [t_min: duration(0,5,0): t_max]';
+t_min = min([min(Tf_wind_ha.TimeStamp), min(Tf_solar_ha.TimeStamp)]);
+t_max = max([max(Tf_wind_ha.TimeStamp), max(Tf_solar_ha.TimeStamp)]);
+time_seq_5m = [t_min: duration(0,5,0): t_max]';
 % selected_load = 'bus1001';
 selected_wind = 'wind0';
 selected_solar = 'solar109';
 
-all_leadtime = [duration(0, 50, 0); duration(0, 55, 0); duration(1, 0, 0)];
+% all_leadtime = [duration(0, 50, 0); duration(0, 55, 0); duration(1, 0, 0)];
+all_leadtime = unique(Tf_wind_ha.leadtime);
 cell_checknan = cell(size(all_leadtime, 1), 1);
 
 for j = 1:size(all_leadtime, 1)
     selected_leadtime = all_leadtime(j);
 
-    genname_wind = T_wind.Properties.VariableNames(contains(T_wind.Properties.VariableNames, 'wind'));
-    T_wind_selected = T_wind(T_wind.leadtime==selected_leadtime, :);
-    [Lia,Locb] = ismember(time_seq, T_wind_selected.TimeStamp);
-    tmp = nan(size(time_seq, 1), numel(genname_wind));
-    tmp(Lia, :) = T_wind_selected{Locb(Lia), genname_wind};
-    T_wind_aligned = [array2table(time_seq, 'VariableNames', {'TIME'}), array2table(tmp, 'VariableNames', genname_wind)];
+    genname_wind = Tf_wind_ha.Properties.VariableNames(contains(Tf_wind_ha.Properties.VariableNames, 'wind'));
+    Tf_wind_ha_selected = Tf_wind_ha(Tf_wind_ha.leadtime==selected_leadtime, :);
+    [Lia,Locb] = ismember(time_seq_5m, Tf_wind_ha_selected.TimeStamp);
+    tmp = nan(size(time_seq_5m, 1), numel(genname_wind));
+    tmp(Lia, :) = Tf_wind_ha_selected{Locb(Lia), genname_wind};
+    Tf_wind_ha_aligned = [array2table(time_seq_5m, 'VariableNames', {'TIME'}), array2table(tmp, 'VariableNames', genname_wind)];
 
-    genname_solar = Tf_solar.Properties.VariableNames(contains(Tf_solar.Properties.VariableNames, 'solar'));
-    Tf_solar_selected = Tf_solar(Tf_solar.leadtime==selected_leadtime, :);
-    [Lia,Locb] = ismember(time_seq, Tf_solar_selected.TimeStamp);
-    tmp = nan(size(time_seq, 1), numel(genname_solar));
-    tmp(Lia, :) = Tf_solar_selected{Locb(Lia), genname_solar};
-    Tf_solar_aligned = [array2table(time_seq, 'VariableNames', {'TIME'}), array2table(tmp, 'VariableNames', genname_solar)];
+    genname_solar = Tf_solar_ha.Properties.VariableNames(contains(Tf_solar_ha.Properties.VariableNames, 'solar'));
+    Tf_solar_ha_selected = Tf_solar_ha(Tf_solar_ha.leadtime==selected_leadtime, :);
+    [Lia,Locb] = ismember(time_seq_5m, Tf_solar_ha_selected.TimeStamp);
+    tmp = nan(size(time_seq_5m, 1), numel(genname_solar));
+    tmp(Lia, :) = Tf_solar_ha_selected{Locb(Lia), genname_solar};
+    Tf_solar_ha_aligned = [array2table(time_seq_5m, 'VariableNames', {'TIME'}), array2table(tmp, 'VariableNames', genname_solar)];
     
 %     genname_load = T_load.Properties.VariableNames(contains(T_load.Properties.VariableNames, 'bus'));
 %     T_load_selected = T_load(T_load.leadtime==selected_leadtime, :);
@@ -94,7 +100,7 @@ for j = 1:size(all_leadtime, 1)
 %     tmp(Lia, :) = T_load_selected{Locb(Lia), genname_load};
 %     T_load_aligned = [array2table(time_seq, 'VariableNames', {'TIME'}), array2table(tmp, 'VariableNames', genname_load)];
 
-    unique_dates = unique([time_seq.Year, time_seq.Month, time_seq.Day], 'rows');
+    unique_dates = unique([time_seq_5m.Year, time_seq_5m.Month, time_seq_5m.Day], 'rows');
     n_nan = zeros(size(unique_dates, 1), 3);
     for i = 1:size(unique_dates, 1)
         this_y = unique_dates(i, 1);
@@ -104,19 +110,64 @@ for j = 1:size(all_leadtime, 1)
 %         rows_select = (T_load_aligned.TIME.Year==this_y)&(T_load_aligned.TIME.Month==this_m)&(T_load_aligned.TIME.Day==this_d);
 %         n_nan(i, 1) = sum(isnan(T_load_aligned{rows_select, selected_load}));
 
-        rows_select = (T_wind_aligned.TIME.Year==this_y)&(T_wind_aligned.TIME.Month==this_m)&(T_wind_aligned.TIME.Day==this_d);
-        n_nan(i, 2) = sum(isnan(T_wind_aligned{rows_select, selected_wind}));
+        rows_select = (Tf_wind_ha_aligned.TIME.Year==this_y)&(Tf_wind_ha_aligned.TIME.Month==this_m)&(Tf_wind_ha_aligned.TIME.Day==this_d);
+        n_nan(i, 2) = sum(isnan(Tf_wind_ha_aligned{rows_select, selected_wind}));
 
-        rows_select = (Tf_solar_aligned.TIME.Year==this_y)&(Tf_solar_aligned.TIME.Month==this_m)&(Tf_solar_aligned.TIME.Day==this_d);
-        n_nan(i, 3) = sum(isnan(Tf_solar_aligned{rows_select, selected_solar}));
+        rows_select = (Tf_solar_ha_aligned.TIME.Year==this_y)&(Tf_solar_ha_aligned.TIME.Month==this_m)&(Tf_solar_ha_aligned.TIME.Day==this_d);
+        n_nan(i, 3) = sum(isnan(Tf_solar_ha_aligned{rows_select, selected_solar}));
     end
 
     T_checknan = array2table([unique_dates, n_nan], 'VariableNames', {'Year', 'Month', 'Day', 'load', 'wind', 'solar'});
     cell_checknan{j} = T_checknan;
 end
 
+% Display all dates with nan
+for j = 1:size(all_leadtime, 1)
+    disp(all_leadtime(j));
+    T_checknan = cell_checknan{j};
+    T_checknan(sum(T_checknan{:, 4:6}, 2)~=0, :)
+end
+
+%% Prepare DA, HA and actual data. Note: HA has selected a leadtime, say 1 hour
+selected_leadtime = duration(1, 0, 0);
+t_min_da = min([min(Tf_wind_da.INTERVAL_ENDING)]);
+t_max_da = max([max(Tf_wind_da.INTERVAL_ENDING)]);
+time_seq_da = [t_min_da: duration(1,0,0): t_max]';
+
+% Solar actual
 Ta_solar_selected = Ta_solar(Ta_solar.leadtime==selected_leadtime, :);
-[Lia,Locb] = ismember(time_seq, Ta_solar_selected.TimeStamp);
-tmp = nan(size(time_seq, 1), numel(genname_solar));
+[Lia,Locb] = ismember(time_seq_5m, Ta_solar_selected.TimeStamp);
+tmp = nan(size(time_seq_5m, 1), numel(genname_solar));
 tmp(Lia, :) = Ta_solar_selected{Locb(Lia), genname_solar};
-Ta_solar_aligned = [array2table(time_seq, 'VariableNames', {'TIME'}), array2table(tmp, 'VariableNames', genname_solar)];
+Ta_solar_aligned = [array2table(time_seq_5m, 'VariableNames', {'TIME'}), array2table(tmp, 'VariableNames', genname_solar)];
+
+% Solar HA forecast
+Tf_solar_ha_selected = Tf_solar_ha(Tf_solar_ha.leadtime==selected_leadtime, :);
+[Lia,Locb] = ismember(time_seq_5m, Tf_solar_ha_selected.TimeStamp);
+tmp = nan(size(time_seq_5m, 1), numel(genname_solar));
+tmp(Lia, :) = Tf_solar_ha_selected{Locb(Lia), genname_solar};
+Tf_solar_ha_aligned = [array2table(time_seq_5m, 'VariableNames', {'TIME'}), array2table(tmp, 'VariableNames', genname_solar)];
+
+% Wind actual
+[Lia,Locb] = ismember(time_seq_5m, Ta_wind.INTERVAL_ENDING);
+tmp = nan(size(time_seq_5m, 1), numel(genname_wind));
+tmp(Lia, :) = Ta_wind{Locb(Lia), genname_wind};
+Ta_wind_aligned = [array2table(time_seq_5m, 'VariableNames', {'TIME'}), array2table(tmp, 'VariableNames', genname_wind)];
+
+% Wind HA forecast
+Tf_wind_ha_selected = Tf_wind_ha(Tf_wind_ha.leadtime==selected_leadtime, :);
+[Lia,Locb] = ismember(time_seq_5m, Tf_wind_ha_selected.TimeStamp);
+tmp = nan(size(time_seq_5m, 1), numel(genname_wind));
+tmp(Lia, :) = Tf_wind_ha_selected{Locb(Lia), genname_wind};
+Tf_wind_ha_aligned = [array2table(time_seq_5m, 'VariableNames', {'TIME'}), array2table(tmp, 'VariableNames', genname_wind)];
+
+% Wind DA forecast
+[Lia,Locb] = ismember(time_seq_da, Tf_wind_da.INTERVAL_ENDING);
+tmp = nan(size(time_seq_da, 1), numel(genname_wind));
+tmp(Lia, :) = Tf_wind_da{Locb(Lia), genname_wind};
+Tf_wind_da_aligned = [array2table(time_seq_da, 'VariableNames', {'TIME'}), array2table(tmp, 'VariableNames', genname_wind)];
+
+% Take a look at the complete sets of wind forecast
+stairs(Tf_wind_da_aligned.TIME-duration(1, 0, 0), Tf_wind_da_aligned.wind0, 'g');
+hold on; stairs(Ta_wind_aligned.TIME-duration(0, 5, 0), Ta_wind_aligned.wind0, 'k');
+stairs(Tf_wind_ha_aligned.TIME-duration(0, 5, 0), Tf_wind_ha_aligned.wind0, 'b');
